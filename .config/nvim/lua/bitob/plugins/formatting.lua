7c0c890de2759059d87a520eac3a6f35cc5f416a
Tobias Bich <87118266+bitob@users.noreply.github.com>
2024-04-26 19:13:33 +0200

    Disable format on save since I have to edit files with weird formatting

--- .config/nvim/lua/bitob/plugins/formatting.lua
+++ .config/nvim/lua/bitob/plugins/formatting.lua
@@ -16,11 +16,12 @@ return {
 				-- sh = { "shfmt" },
 				sh = { "beautysh" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			format_on_save = false,
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
