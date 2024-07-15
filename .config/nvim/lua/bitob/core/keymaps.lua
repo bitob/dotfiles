2662833e6f70a16a792e6adce5c4fa604efa05b0
Tobias Bich <87118266+bitob@users.noreply.github.com>
2024-07-15 12:42:46 +0200

    Add keymaps for navigating visual lines

--- .config/nvim/lua/bitob/core/keymaps.lua
+++ .config/nvim/lua/bitob/core/keymaps.lua
@@ -2,15 +2,30 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
+keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down by visual Line", expr = true, silent = true })
+keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up by visual line", expr = true, silent = true })
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
+
+-- -- Navigate through the quickfix and location list
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 keymap.set("n", "<CR>", "o<ESC>", { noremap = true, silent = true, desc = "Insert blank line below" })
 -- TODO: This won't work, probably because the terminal doesn't recognize Shift + Enter correctly
 keymap.set("n", "<S-CR>", "O<ESC>", { noremap = true, silent = true, desc = "Insert blank line above" })
 
 keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+
+keymap.set(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace word under the cursor" }
+)
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
@@ -35,11 +50,16 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 -- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 -- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-keymap.set("n", "<leader>io", "i\
+keymap.set(
+	"n",
+	"<leader>io",
+	"i\
 // Copyright Open Logistics Foundation\
 //\
 // Licensed under the Open Logistics Foundation License 1.3.\
 // For details on the licensing terms, see the LICENSE file.\
 // SPDX-License-Identifier: OLFL-1.3\
 //\
-<ESC>", { desc = "Insert OLFL-1.3 License Header" })
+<ESC>",
+	{ desc = "Insert OLFL-1.3 License Header" }
+)
