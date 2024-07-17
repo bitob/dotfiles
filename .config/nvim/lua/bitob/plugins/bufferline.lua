c2b284241e4f00aab2b0a00203d24a4af3609947
Tobias Bich <87118266+bitob@users.noreply.github.com>
2024-07-17 10:41:19 +0200

    Update buffer navigation keymaps

--- .config/nvim/lua/bitob/plugins/bufferline.lua
+++ .config/nvim/lua/bitob/plugins/bufferline.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			local bufferline = require("bufferline")
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
 
-			bufferline.setup({
-				options = {},
-			})
+		bufferline.setup({
+			options = {},
+		})
 
-			vim.keymap.set("n", "<leader>bb", "<CMD>BufferLinePick<CR>")
-			vim.keymap.set("n", "<leader>ts", "<CMD>BufferLinePickClose<CR>")
-			vim.keymap.set("n", "<C-L>", "<CMD>BufferLineCycleNext<CR>")
-			vim.keymap.set("n", "<C-H>", "<CMD>BufferLineCyclePrev<CR>")
-			vim.keymap.set("n", "]b", "<CMD>BufferLineMoveNext<CR>")
-			vim.keymap.set("n", "[b", "<CMD>BufferLineMovePrev<CR>")
-			vim.keymap.set("n", "<leader>bs", "<CMD>BufferLineSortByDirectory<CR>")
-			vim.keymap.set("n", "<leader>bc", "<CMD>BufferLineCloseOthers<CR>")
-		end,
-	},
+		vim.keymap.set("n", "<leader>bb", "<CMD>BufferLinePick<CR>")
+		vim.keymap.set("n", "<leader>ts", "<CMD>BufferLinePickClose<CR>")
+		vim.keymap.set("n", "<C-n>", "<CMD>BufferLineCycleNext<CR>")
+		vim.keymap.set("n", "<C-p>", "<CMD>BufferLineCyclePrev<CR>")
+		vim.keymap.set("n", "<Tab>", "<CMD>BufferLineCycleNext<CR>")
+		vim.keymap.set("n", "<S-Tab>", "<CMD>BufferLineCyclePrev<CR>")
+		vim.keymap.set("n", "]b", "<CMD>BufferLineMoveNext<CR>")
+		vim.keymap.set("n", "[b", "<CMD>BufferLineMovePrev<CR>")
+		vim.keymap.set("n", "<leader>bs", "<CMD>BufferLineSortByDirectory<CR>")
+		vim.keymap.set("n", "<leader>bc", "<CMD>BufferLineCloseOthers<CR>")
+	end,
 }
